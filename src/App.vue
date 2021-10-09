c7161d1df7fe194b03e527dbfc1865ae20a29281
Fahim <fahimalemroz@gmail.com>
2021-10-09 08:04:43 +0600

    Gsap works

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,133 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue';
-</script>
-
 <template>
-	<img alt="Vue logo" src="./assets/logo.png" />
+	<img v-if="false" alt="Vue logo" src="./assets/logo.png" />
 	<HelloWorld msg="Hello Vue 3 + Vite" />
-	<p>'Inky pinkies per donkey monkeys'</p>
+
+	<transition appear @before-enter="beforeEnter">
+		<div id="buttons w-1/3 my-0 mx-auto text-center">
+			<button
+				class="
+					block
+					bg-green-500
+					font-bold
+					text-white
+					py-2
+					px-4
+					mb-2
+					rounded-2xl
+					shadow
+				"
+			>
+				Inky pinkies per donkey monkeys
+			</button>
+			<button
+				class="
+					block
+					bg-green-500
+					font-bold
+					text-white
+					py-2
+					px-4
+					mb-2
+					rounded-2xl
+					shadow
+				"
+			>
+				Inky pinkies per donkey monkeys</button
+			><button
+				class="
+					block
+					bg-green-500
+					font-bold
+					text-white
+					py-2
+					px-4
+					mb-2
+					rounded-2xl
+					shadow
+				"
+			>
+				Inky pinkies per donkey monkeys</button
+			><button
+				class="
+					block
+					bg-green-500
+					font-bold
+					text-white
+					py-2
+					px-4
+					mb-2
+					rounded-2xl
+					shadow
+				"
+			>
+				Inky pinkies per donkey monkeys</button
+			><button
+				class="
+					block
+					bg-green-500
+					font-bold
+					text-white
+					py-2
+					px-4
+					mb-2
+					rounded-2xl
+					shadow
+				"
+			>
+				Inky pinkies per donkey monkeys</button
+			><button
+				class="
+					block
+					bg-green-500
+					font-bold
+					text-white
+					py-2
+					px-4
+					mb-2
+					rounded-2xl
+					shadow
+				"
+			>
+				Inky pinkies per donkey monkeys</button
+			><button
+				class="
+					block
+					bg-green-500
+					font-bold
+					text-white
+					py-2
+					px-4
+					mb-2
+					rounded-2xl
+					shadow
+				"
+			>
+				Inky pinkies per donkey monkeys
+			</button>
+		</div>
+	</transition>
 </template>
 
+<script>
+import gsap from 'gsap';
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+	methods: {
+		beforeEnter(el) {
+			gsap.from(el.children, {
+				scale: 0.6,
+				yPercent: 50,
+				ease: 'power2.in',
+				opacity: 0,
+				delay: 1,
+				stagger: 0.3,
+				duration: 0.3,
+			});
+		},
+	},
+};
+</script>
 <style>
 #app {
 	font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -18,5 +136,6 @@ import HelloWorld from './components/HelloWorld.vue';
 	text-align: center;
 	color: #2c3e50;
 	margin-top: 60px;
+	font-family: 'Source Code Pro';
 }
 </style>
