1698960da76a445119b244f4fd26078dd6514f2a
Fahim <fahimalemroz@gmail.com>
2022-01-07 17:20:34 +0600

    removed all content from app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,13 @@
-<template>
-	<img v-if="false" alt="Vue logo" src="./assets/logo.png" />
-	<HelloWorld msg="Hello Vue 3 + Vite" />
-
-	<transition appear @before-enter="beforeEnter">
-		<h1 class="text-center my-10 text-9xl text-gray-800 font-bold">
-			Coming Soon...
-		</h1>
-	</transition>
-</template>
+<template></template>
 
 <script>
 import gsap from 'gsap';
 import HelloWorld from './components/HelloWorld.vue';
 
 export default {
-	methods: {
-		beforeEnter(el) {
-			gsap.from(el.children, {
-				scale: 0.6,
-				yPercent: 50,
-				ease: 'power2.in',
-				opacity: 0,
-				delay: 1,
-				stagger: 0.3,
-				duration: 0.3,
-			});
-		},
+	name: 'App',
+	setup() {
+		console.log('car');
 	},
 };
 </script>
