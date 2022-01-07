8babdf3e508f9be9238a554c87921b10d79415ec
Fahim <fahimalemroz@gmail.com>
2022-01-07 21:51:53 +0600

    Created a cursor component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,45 @@
 <template>
-	<Cursor />
+	<button
+		class="
+			text-4xl
+			bg-blue-400
+			rounded
+			shadow
+			py-3
+			px-6
+			text-white
+			font-bold
+			pointer
+		"
+	>
+		Clicc
+	</button>
+	<p class="text">
+		Ipsum voluptas nam nisi rem dicta Est ut veritatis repellendus provident
+		iusto? Doloremque unde placeat possimus dolores totam. Ea consequuntur
+		molestias consectetur animi quaerat rem! Ut nemo fuga impedit sit modi
+		Ipsa illo aliquam ipsam beatae beatae maiores Necessitatibus tempore vero
+		magni aut expedita! Ducimus dolorum delectus quidem commodi error.
+	</p>
 </template>
 
 <script>
-import Cursor from './cursor.vue';
-
 export default {
 	name: 'App',
-	components: { Cursor },
 };
 </script>
 
 <style>
 body {
-	cursor: none !important;
+	border: 1px solid red;
+	cursor: url('./assets/cursor.svg'), auto;
+}
+
+.pointer {
+	cursor: url('./assets/pointer.svg'), auto;
+}
+
+.text {
+	cursor: url('./assets/text.svg'), auto;
 }
 </style>
