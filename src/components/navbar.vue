df3fc4784a5b22d0ee2e0458d877997ada53af37
CoderLadFahim <fahimalemroz@gmail.com>
2022-01-09 10:25:16 +0600

    Added navbar component file and installed fontawesome libraries for vue 3

---
+++ src/components/navbar.vue
@@ -0,0 +1,20 @@
+<template>
+	<nav>
+		<div class="nav-item">
+			<div></div>
+			<p></p>
+		</div>
+		<div class="nav-item">
+			<div></div>
+			<p></p>
+		</div>
+		<div class="nav-item">
+			<div></div>
+			<p></p>
+		</div>
+	</nav>
+</template>
+
+<script></script>
+
+<style></style>
