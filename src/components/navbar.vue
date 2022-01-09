9b68a3ca9fc0cc2ec3c8235ff775b5c2077bbf6e
CoderLadFahim <fahimalemroz@gmail.com>
2022-01-09 12:56:18 +0600

    Started work on desktop navbar

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,27 +1,5 @@
 <template>
-	<nav
-		class="
-			border
-			flex
-			ubuntu-mono
-			shadow
-			justify-between
-			w-11/12
-			px-4
-			py-1
-			rounded-xl
-			fixed
-			left-1/2
-			bottom-3
-			transform
-			-translate-x-1/2
-			bg-white
-			sm:bottom-auto
-			sm:top-3
-			sm:w-3/5
-			md:w-1/2
-		"
-	>
+	<nav class="nav-styles ubuntu-mono">
 		<div class="nav-item pointer">
 			<font-awesome-icon icon="home" />
 			<p>Home</p>
@@ -44,13 +22,17 @@
 <script></script>
 
 <style>
+.nav-styles {
+	@apply border flex shadow justify-between w-11/12 px-4 py-1 rounded-xl fixed left-1/2 bottom-3 transform -translate-x-1/2 bg-white sm:bottom-auto sm:top-3 sm:w-3/5 md:w-1/2 xl:rotate-90 xl:translate-x-0 xl:py-2 xl:top-1/2 xl:left-12 xl:-translate-x-1/2;
+}
+
 .active {
-	color: #A5A6F6 !important;
+	color: #a5a6f6 !important;
 	font-weight: bold;
 }
 
 .nav-item {
-	@apply text-center text-gray-400;
+	@apply text-center text-gray-400 xl:transform xl:-rotate-90;
 }
 
 .nav-item svg {
