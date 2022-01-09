06224168679ca53493fd66c1ec786f29ccf56c83
CoderLadFahim <fahimalemroz@gmail.com>
2022-01-09 12:01:56 +0600

    Navbar created for xxs

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,18 +1,35 @@
 <template>
-	<nav class="border flex justify-between ubuntu-mono">
-		<div class="nav-item">
+	<nav
+		class="
+			border
+			flex
+			ubuntu-mono
+			shadow
+			justify-between
+			w-11/12
+			px-4
+			py-1
+			rounded-xl
+			fixed
+			left-1/2
+			bottom-3
+			transform
+			-translate-x-1/2
+		"
+	>
+		<div class="nav-item pointer">
 			<font-awesome-icon icon="home" />
 			<p>Home</p>
 		</div>
-		<div class="nav-item">
+		<div class="nav-item pointer">
 			<font-awesome-icon icon="code" />
 			<p>Skills</p>
 		</div>
-		<div class="nav-item">
+		<div class="nav-item pointer">
 			<font-awesome-icon icon="project-diagram" />
 			<p>Project</p>
 		</div>
-		<div class="nav-item">
+		<div class="nav-item pointer">
 			<font-awesome-icon icon="phone" />
 			<p>Contact</p>
 		</div>
@@ -24,5 +41,9 @@
 <style>
 .nav-item {
 	@apply text-center text-gray-400;
+}
+
+.nav-item p {
+	@apply text-xs;
 }
 </style>
