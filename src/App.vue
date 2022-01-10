26c5a77dab2fc496430f7792d8bc156731764823
CoderLadFahim <fahimalemroz@gmail.com>
2022-01-10 12:40:59 +0600

    Removed the dummy text and btn from App component, navbar styled for mobile, tablets and desktops, lacks functionality

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,9 @@
 <template>
-	<button
-		class="
-			text-4xl
-			bg-blue-400
-			rounded
-			shadow
-			py-3
-			px-6
-			text-white
-			font-bold
-			pointer
-		"
-	>
-		Clicc
-	</button>
-	<p class="text">
-		Ipsum voluptas nam nisi rem dicta Est ut veritatis repellendus provident
-		iusto? Doloremque unde placeat possimus dolores totam. Ea consequuntur
-		molestias consectetur animi quaerat rem! Ut nemo fuga impedit sit modi
-		Ipsa illo aliquam ipsam beatae beatae maiores Necessitatibus tempore vero
-		magni aut expedita! Ducimus dolorum delectus quidem commodi error.
-	</p>
-
 	<Navbar />
 </template>
 
 <script>
-import Navbar from './components/Navbar.vue';
+import Navbar from './components/navbar.vue';
 
 export default {
 	name: 'App',
@@ -36,8 +13,7 @@ export default {
 
 <style>
 body {
-	@apply bg-gray-100;
-	border: 1px solid red;
+	@apply bg-gray-100 border border-4 border-blue-600;
 	cursor: url('./assets/cursor.svg'), auto;
 	min-height: 100vh;
 }
