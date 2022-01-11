2a49ac312afb453e7ebae4c47999f8184fbb1374
CoderLadFahim <fahimalemroz@gmail.com>
2022-01-11 18:39:17 +0600

    Navbar restyled for desktop screens

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -23,7 +23,7 @@
 
 <style>
 .nav-styles {
-	@apply border flex shadow justify-between w-11/12 px-4 py-1 rounded-xl fixed left-1/2 bottom-3 transform -translate-x-1/2 bg-white sm:bottom-auto sm:top-3 sm:w-3/5 md:w-1/2 md:py-2 md:px-6 md:rounded-2xl lg:scale-110 lg:top-4 lg:py-3 lg:rounded-2xl xl:flex-col xl:w-16 xl:h-1/2 xl:top-1/2 xl:-translate-y-1/2 xl:left-14;
+	@apply border flex shadow justify-between w-11/12 px-4 py-1 rounded-xl fixed left-1/2 bottom-3 transform -translate-x-1/2 bg-white sm:bottom-auto sm:top-3 sm:w-3/5 md:w-1/2 md:py-2 md:px-6 md:rounded-2xl lg:scale-110 lg:top-4 lg:py-3 lg:rounded-2xl xl:flex-col xl:w-16 xl:h-1/2 xl:top-1/2 xl:-translate-y-1/2 xl:left-14 xl:py-8 xl:px-9 2xl:h-3/5;
 }
 
 .nav-item.active {
@@ -32,11 +32,11 @@
 }
 
 .nav-item {
-	@apply text-center text-gray-400 xl:flex xl:flex-col xl:items-center xl:gap-2;
+	@apply text-center text-gray-400 transition duration-200 hover:text-gray-500 xl:flex xl:flex-col xl:items-center xl:gap-2;
 }
 
 .nav-item svg {
-	@apply w-44 md:transform md:scale-125;
+	@apply w-44 md:transform md:scale-125 xl:scale-150;
 }
 
 .nav-item p {
