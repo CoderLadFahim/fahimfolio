301fb10ed83ef61e41a7a24d009d02a8fae64ca2
Fahim <fahimalemroz@gmail.com>
2022-01-14 15:23:35 +0600

    Added additional content to Contact.vue

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -2,15 +2,58 @@
 	<section id="contact-section">
 		<section-title titleText="Contact.Me" />
 	</section>
+
+	<form>
+		<app-input></app-input>
+		<app-input></app-input>
+		<app-input></app-input>
+
+		<textarea
+			id=""
+			name=""
+			cols="30"
+			rows="10"
+			placeholder="Your message here"
+		></textarea>
+
+		<button
+			@click.enter="submitButtonHandler"
+			class="
+				bg-purple-400
+				hover:bg-purple-300
+				transition
+				pointer
+				text-white
+				fira-code-bold
+				shadow
+				rounded-lg
+				py-2
+				px-4
+			"
+		>
+			Get In Touch!
+		</button>
+	</form>
 </template>
 
 <script>
 import SectionTitle from '../components/SectionTitle.vue';
+import Input from '../components/Input.vue';
 
 export default {
 	name: 'Contact',
 	components: {
 		'section-title': SectionTitle,
+		'app-input': Input,
+	},
+	setup() {
+		const submitButtonHandler = (e) => {
+			e.preventDefault();
+		};
+
+		return {
+			submitButtonHandler,
+		};
 	},
 };
 </script>
