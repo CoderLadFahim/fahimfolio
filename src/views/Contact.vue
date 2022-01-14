b435bbcb30c57801b85fe4965ed7cef4a8f53880
Fahim <fahimalemroz@gmail.com>
2022-01-14 19:10:37 +0600

    Added some basic styles to inputs

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,51 +1,40 @@
 <template>
-	<section id="contact-section">
+	<section id="contact-section" class="w-11/12 flex flex-col items-center">
 		<section-title titleText="Contact.Me" />
+
+		<form
+			action="https://formsubmit.co/fahimalemroz@gmail.com"
+			method="post"
+			class="mt-7 flex flex-col items-center space-y-6"
+		>
+			<app-input labelText="Your name" idMatcher="name" />
+			<app-input labelText="Your business name" idMatcher="business" />
+			<app-input labelText="Email" inputType="email" idMatcher="email" />
+
+			<textarea
+				class="rounded-lg py-2 px-3"
+				name="visitor-message"
+				cols="25"
+				rows="9"
+				placeholder="Your message here"
+			></textarea>
+
+			<input
+				type="hidden"
+				name="_next"
+				value="https://fahimfolio.netlify.app"
+			/>
+
+			<input type="hidden" name="_captcha" value="false" />
+
+			<input
+				type="submit"
+				@click="(e) => e.preventDefault()"
+				value="Get In Touch!"
+				class="submit-btn fira-code-bold pointer"
+			/>
+		</form>
 	</section>
-
-	<form
-		action="https://formsubmit.co/fahimalemroz@gmail.com"
-		method="post"
-		class="space-y-4"
-	>
-		<app-input labelText="Your name" idMatcher="name" />
-		<app-input labelText="Your business name" idMatcher="business" />
-		<app-input labelText="Email" inputType="email" idMatcher="email" />
-
-		<textarea
-			id=""
-			name="visitor-message"
-			cols="30"
-			rows="10"
-			placeholder="Your message here"
-		></textarea>
-
-		<input
-			type="hidden"
-			name="_next"
-			value="https://fahimfolio.netlify.app"
-		/>
-
-		<input type="hidden" name="_captcha" value="false" />
-
-		<input
-			type="submit"
-			@click="(e) => e.preventDefault()"
-			value="Get In Touch!"
-			class="
-				bg-purple-400
-				hover:bg-purple-300
-				transition
-				pointer
-				text-white
-				fira-code-bold
-				shadow
-				rounded-lg
-				py-2
-				px-4
-			"
-		/>
-	</form>
 </template>
 
 <script>
@@ -61,4 +50,12 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.submit-btn {
+	@apply bg-purple-400 hover:bg-purple-300 transition text-white shadow-md rounded-lg py-2 px-4;
+}
+
+textarea:focus {
+@apply border-purple-300;
+}
+</style>
