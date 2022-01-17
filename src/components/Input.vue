801d469d02a9ed7732d32729f13997c0f036bb7b
CoderLadFahim <fahimalemroz@gmail.com>
2022-01-17 10:37:51 +0600

    Effect added, working on transitions

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -52,12 +52,6 @@ label {
 	@apply absolute  top-0 bottom-0 left-0 right-0 pointer-events-none;
 }
 
-label::after {
-	content: '';
-	@apply inline-block absolute bottom-0 w-0 bg-purple-400;
-	height: 2px;
-}
-
 label span {
 	@apply absolute bottom-0;
 }
@@ -67,8 +61,19 @@ input {
 	@apply w-full h-full bg-transparent  pt-9;
 }
 
+label::after {
+	content: '';
+	@apply inline-block absolute bottom-0 w-0 bg-purple-400 transition;
+	height: 2px;
+}
+
 input:focus + label span,
 input:valid + label span {
 	@apply top-1 text-xs font-bold text-purple-400;
+}
+
+input:focus + label::after,
+input:valid + label::after {
+	@apply w-full;
 }
 </style>
