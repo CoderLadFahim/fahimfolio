4306579d38b9a9224f24403c749d627acd0c1308
Fahim <fahimalemroz@gmail.com>
2022-02-04 19:28:13 +0600

    Started adding markup to Skills

---
+++ src/views/Skills.vue
@@ -0,0 +1,16 @@
+<template>
+	<section id="skills-section">
+		<section-title titleText="My.Skills" />
+		<div class="skill-cols">
+			<h2 class="col-title">Languages</h2>
+			<div class="skills">
+				<img src="" alt="" />
+				<p>HTML+CSS</p>
+			</div>
+		</div>
+	</section>
+</template>
+
+<script></script>
+
+<style></style>
