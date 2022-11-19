4172895492519903802650ea07c565bfd016cdd7
CoderLadFahim <fahimalemroz@gmail.com>
2022-11-19 18:49:19 +0600

    Skills section done and dusted for now for mobile screens

--- src/views/Skills.vue
+++ src/views/Skills.vue
@@ -1,36 +1,106 @@
 <template>
 	<section id="skills-section" class="site-section">
 		<section-title titleText="My.Skills" />
-		<div
-			class="skill-col rounded-md w-5/12 border-2 border-green-300 source-code-pro-bold"
-		>
+		<div class="flex items-start mt-5">
 			<div
-				class="col-header bg-green-300 text-white text-center py-2 px-0 text-xs"
+				class="skill-col bg-gray-50 rounded-md w-5/12 border-2 border-green-300 source-code-pro-bold secondary-col"
 			>
-				Frameworks & Libraries
-			</div>
-			<div class="col-body space-y-6 py-6">
 				<div
-					class="skill space-y-1 flex flex-col justify-center items-center"
-					v-for="skill in frameworks"
-					:key="skill.name"
+					class="col-header bg-green-300 text-white text-center py-2 px-0 text-xs"
 				>
-					<img
-						:src="skill.skillImgPath"
-						:alt="skill.name"
-						:class="`${skill.magnified ? 'w-20' : 'w-12'}`"
-					/>
-					<p
-						:class="`text-center fira-code-medium text-gray-400 ${
-							skill.magnified ? 'text-sm' : 'text-xs'
-						} ${
-							skill.isSeparator
-								? 'pb-4 border-b-2 border-gray-200 w-3/4'
-								: ''
-						}`"
+					<span class="col-title">Languages</span>
+				</div>
+				<div class="col-body space-y-6 py-6">
+					<div
+						class="skill space-y-1 flex flex-col justify-center items-center"
+						v-for="skill in languages"
+						:key="skill.name"
 					>
-						{{ skill.skillName }}
-					</p>
+						<img
+							:src="skill.skillImgPath"
+							:alt="skill.name"
+							:class="`${skill.magnified ? 'w-20' : 'w-12'}`"
+						/>
+						<p
+							:class="`text-center fira-code-medium text-gray-400 ${
+								skill.magnified ? 'text-sm' : 'text-xs'
+							} ${
+								skill.isSeparator
+									? 'pb-4 border-b-2 border-gray-200 w-3/4'
+									: ''
+							}`"
+						>
+							{{ skill.skillName }}
+						</p>
+					</div>
+				</div>
+			</div>
+
+			<div
+				class="skill-col rounded-md w-5/12 border-2 border-green-300 bg-white source-code-pro-bold z-50"
+			>
+				<div
+					class="col-header bg-green-300 text-white text-center py-2 text-xs"
+				>
+					Frameworks
+				</div>
+				<div class="col-body space-y-6 py-6">
+					<div
+						class="skill space-y-1 flex flex-col justify-center items-center"
+						v-for="skill in frameworks"
+						:key="skill.name"
+					>
+						<img
+							:src="skill.skillImgPath"
+							:alt="skill.name"
+							:class="`${skill.magnified ? 'w-20' : 'w-12'}`"
+						/>
+						<p
+							:class="`text-center fira-code-medium text-gray-400 ${
+								skill.magnified ? 'text-sm' : 'text-xs'
+							} ${
+								skill.isSeparator
+									? 'pb-4 border-b-2 border-gray-200 w-3/4'
+									: ''
+							}`"
+						>
+							{{ skill.skillName }}
+						</p>
+					</div>
+				</div>
+			</div>
+
+			<div
+				class="skill-col bg-gray-50 rounded-md w-5/12 border-2 border-green-300 source-code-pro-bold secondary-col"
+			>
+				<div
+					class="col-header bg-green-300 text-white text-center py-2 px-0 text-xs"
+				>
+					<span class="col-title">Tools</span>
+				</div>
+				<div class="col-body space-y-6 py-6">
+					<div
+						class="skill space-y-1 flex flex-col justify-center items-center"
+						v-for="skill in tools"
+						:key="skill.name"
+					>
+						<img
+							:src="skill.skillImgPath"
+							:alt="skill.name"
+							:class="`${skill.magnified ? 'w-20' : 'w-12'}`"
+						/>
+						<p
+							:class="`text-center fira-code-medium text-gray-400 ${
+								skill.magnified ? 'text-sm' : 'text-xs'
+							} ${
+								skill.isSeparator
+									? 'pb-4 border-b-2 border-gray-200 w-3/4'
+									: ''
+							}`"
+						>
+							{{ skill.skillName }}
+						</p>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -82,9 +152,72 @@ export default {
 			},
 		];
 
-		return { frameworks };
+		const languages = [
+			{
+				skillName: 'HTML+CSS',
+				skillImgPath: '/src/assets/TechLogos/css.png',
+				magnified: false,
+			},
+			{
+				skillName: 'Sass',
+				skillImgPath: '/src/assets/TechLogos/sass.png',
+				magnified: false,
+			},
+			{
+				skillName: 'JavaScript',
+				skillImgPath: '/src/assets/TechLogos/JS.png',
+				magnified: false,
+			},
+			{
+				skillName: 'TypeScript',
+				skillImgPath: '/src/assets/TechLogos/TS.png',
+				magnified: false,
+			},
+		];
+
+		const tools = [
+			{
+				skillName: 'Terminal',
+				skillImgPath: '/src/assets/TechLogos/terminal.png',
+				magnified: false,
+			},
+			{
+				skillName: 'Git',
+				skillImgPath: '/src/assets/TechLogos/git.png',
+				magnified: false,
+			},
+			{
+				skillName: 'GitHub',
+				skillImgPath: '/src/assets/TechLogos/github.png',
+				magnified: false,
+			},
+			{
+				skillName: 'npm',
+				skillImgPath: '/src/assets/TechLogos/npm.png',
+				magnified: false,
+			},
+			{
+				skillName: 'figma',
+				skillImgPath: '/src/assets/TechLogos/figma.png',
+				magnified: false,
+			},
+		];
+
+		return { frameworks, languages, tools };
 	},
 };
 </script>
 
-<style></style>
+<style>
+.secondary-col {
+	@apply mt-8;
+}
+
+.secondary-col:first-child {
+	@apply transform translate-x-4;
+}
+
+.secondary-col:last-child {
+	@apply transform -translate-x-4;
+}
+</style>
