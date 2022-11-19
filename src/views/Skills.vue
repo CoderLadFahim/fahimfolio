2e47e5d67efa90fd292d6c41fde5d0894f128e51
CoderLadFahim <fahimalemroz@gmail.com>
2022-11-19 11:21:20 +0600

    Added a basic skill column in the skills section

--- src/views/Skills.vue
+++ src/views/Skills.vue
@@ -1,13 +1,26 @@
 <template>
 	<section id="skills-section" class="site-section">
 		<section-title titleText="My.Skills" />
-
-		<div class="skill-cols">
-			<div class="skill-col">
-				<h2 class="col-title">Languages</h2>
-				<div class="skills">
-					<img src="" alt="" />
-					<p>HTML+CSS</p>
+		<div
+			class="skill-col rounded-md w-5/12 border-2 border-green-300 source-code-pro-bold"
+		>
+			<div class="col-header bg-green-300 text-white text-center py-2">
+				Languages
+			</div>
+			<div class="col-body">
+				<div
+					class="skill space-y-1 border flex flex-col justify-center items-center"
+					v-for="skill in skills"
+					:key="skill.name"
+				>
+					<img
+						:src="skill.skillImgPath"
+						:alt="skill.name"
+						:class="`border ${skill.magnified ? 'w-40' : 'w-32'}`"
+					/>
+					<p class="text-center fira-code-medium text-gray-400 border">
+						{{ skill.skillName }}
+					</p>
 				</div>
 			</div>
 		</div>
@@ -15,12 +28,36 @@
 </template>
 
 <script>
+import { ref } from 'vue';
 import SectionTitle from '../components/SectionTitle.vue';
+
+/* <!-- '/src/assets/SlideShowTechLogos/React.png', --> */
 
 export default {
 	name: 'SkillsSection',
 	components: {
 		'section-title': SectionTitle,
+	},
+	setup() {
+		const skills = ref([
+			{
+				skillName: 'Vue',
+				skillImgPath: '/src/assets/TechLogos/Vue-Logo.png',
+				magnified: true,
+			},
+			{
+				skillName: 'VueX',
+				skillImgPath: '/src/assets/TechLogos/vuex.png',
+				magnified: false,
+			},
+			{
+				skillName: 'ReactJS',
+				skillImgPath: '/src/assets/TechLogos/React-Logo.png',
+				magnified: true,
+			},
+		]);
+
+		return { skills };
 	},
 };
 </script>
